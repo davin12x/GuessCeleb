40260f8c5e7c475298f481deec6f1a2c6ad6ea7e
Lalit Bagga <Davin12x@Lalits-MacBook-Pro.local>
2016-04-06 20:50:57 -0400

    Data Fecthed from internet

--- app/src/main/java/com/example/bagga/guesscelebs/ViewController.java
+++ app/src/main/java/com/example/bagga/guesscelebs/ViewController.java
@@ -1,13 +1,78 @@
 package com.example.bagga.guesscelebs;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ViewController extends AppCompatActivity {
 
+    ArrayList<String> name = new ArrayList<String>();
+    ArrayList<String> imagePath = new ArrayList<String>();
+    public class Service extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+            StringBuilder result = new StringBuilder();
+            URL url = null;
+            HttpURLConnection connection = null;
+
+            try{
+                url = new URL(urls[0]);
+                connection =(HttpURLConnection) url.openConnection();
+                InputStream inputStream = connection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+                int data = reader.read();
+                while (data != -1){
+                    char current = (char)data;
+                    result.append(current);
+                    data = reader.read();
+                }
+                return result.toString();
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_controller);
+        Service downloadTask = new Service();
+        String result = "";
+        try {
+            result = downloadTask.execute("http://www.posh24.com/celebrities").get();
+            String[] splitResult = result.split("<div class=\"sidebarContainer\">");
+            Pattern p = Pattern.compile("<img src=\"(.*?)\"");
+            Matcher m = p.matcher(splitResult[0]);
+            while (m.find()){
+                //ImagePath
+                imagePath.add(m.group(1));
+            }
+            p = Pattern.compile("alt=\"(.*?)\"");
+            m = p.matcher(splitResult[0]);
+            while (m.find()) {
+                //Celeb Names
+                name.add(m.group(1));
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
     }
 }
