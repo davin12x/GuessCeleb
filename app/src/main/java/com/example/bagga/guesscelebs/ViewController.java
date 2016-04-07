9be47e5131d26b32329b042c5df3dd425cddddd0
Lalit <davin12x@gmail.com>
2016-04-06 21:31:22 -0400

    Circle Image Gradle Added

--- app/src/main/java/com/example/bagga/guesscelebs/ViewController.java
+++ app/src/main/java/com/example/bagga/guesscelebs/ViewController.java
@@ -1,15 +1,20 @@
 package com.example.bagga.guesscelebs;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,13 +23,39 @@ public class ViewController extends AppCompatActivity {
 
     ArrayList<String> name = new ArrayList<String>();
     ArrayList<String> imagePath = new ArrayList<String>();
-    public class Service extends AsyncTask<String,Void,String>{
+    int randomImage = 0;
+    ImageView imageView;
+//    Button bb = (Button)findViewById(R.id.button);
+//    Button b1 = (Button)findViewById(R.id.button1);
+//    Button b2 = (Button)findViewById(R.id.button2);
+//    Button b3 = (Button)findViewById(R.id.button3);
+    URL url = null;
+    HttpURLConnection connection = null;
 
+    public class DownloadImage extends AsyncTask<String,Void,Bitmap>{
+
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try{
+                url = new URL(urls[0]);
+                connection = (HttpURLConnection)url.openConnection();
+                InputStream inputStream = connection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+            }
+
+            catch(Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+    public class Service extends AsyncTask<String,Void,String>{
         @Override
         protected String doInBackground(String... urls) {
             StringBuilder result = new StringBuilder();
-            URL url = null;
-            HttpURLConnection connection = null;
+
 
             try{
                 url = new URL(urls[0]);
@@ -37,6 +68,7 @@ public class ViewController extends AppCompatActivity {
                     result.append(current);
                     data = reader.read();
                 }
+
                 return result.toString();
             }
             catch (Exception e){
@@ -50,6 +82,7 @@ public class ViewController extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_controller);
+        imageView = (ImageView) findViewById(R.id.celeb);
         Service downloadTask = new Service();
         String result = "";
         try {
@@ -66,13 +99,22 @@ public class ViewController extends AppCompatActivity {
             while (m.find()) {
                 //Celeb Names
                 name.add(m.group(1));
+                Result();
             }
-
+            Random random = new Random();
+            randomImage = random.nextInt(imagePath.size());
+            DownloadImage imageDownloader = new DownloadImage();
+            Bitmap celebImage;
+            celebImage = imageDownloader.execute(imagePath.get(randomImage)).get();
+            imageView.setImageBitmap(celebImage);
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
+
+    }
+    public void Result(){
 
     }
 }
